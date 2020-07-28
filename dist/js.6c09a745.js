b14aa99f4c53ebcbfeb97d6317f5e5ec8d932043
Victor <justicejesus1237@gmail.com>
2020-07-28 15:14:53 +0300

    add favicon

--- dist/js.6c09a745.js
+++ dist/js.6c09a745.js
@@ -19358,7 +19358,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50126" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62867" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
