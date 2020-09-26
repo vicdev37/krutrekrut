ed3e482a5cf2134b8fd76b377caccc756373790d
Victor <justicejesus1237@gmail.com>
2020-09-26 13:15:17 +0300

    eng vers done

--- dist/js.6c09a745.js
+++ dist/js.6c09a745.js
@@ -19358,7 +19358,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52478" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55221" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
